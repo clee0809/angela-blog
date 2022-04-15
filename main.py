c919be6af741a062842c99b5b5cc5e051c7dbf6f
Hyewon Lee <2017poplj@gmail.com>
2022-04-15 00:20:32 -0700

    Upgrade to PostgreSQL

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///blog.db') # postgreSQL for production, sqlite for development
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
